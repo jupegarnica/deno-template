0b65dd56386941cb99f0e22337fec076c7f0b461
Jupegarnica <juan@geekshubs.com>
2022-03-19 11:56:57 +0100

    test: autoformat

--- _scripts/auto_format.sh
+++ _scripts/auto_format.sh
@@ -12,6 +12,7 @@ FILES=$(git diff --staged --name-only --diff-filter=ACMR "*.*")
 # if files are changed, commit them
 
 if [ -n "$FILES" ]; then
+    echo "Committing changes..."
     export VR_HOOKS="false"
     git add $FILES
     git commit -a -m "chore: Apply auto format"
