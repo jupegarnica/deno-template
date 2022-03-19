385463eafce8f9d30143fcfeb50affd929de553d
Jupegarnica <juan@geekshubs.com>
2022-03-19 11:37:49 +0100

    feat: exclude fmt

--- test/server.test.ts
+++ test/server.test.ts
@@ -1,15 +1,13 @@
 // test server api rest with fetch
 
-const { test } = Deno;
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.130.0/testing/asserts.ts";
 
-console.log(Deno.env.get("TEST_SERVER"));
-test({
+Deno.test({
   name: "[server] test api rest",
   ignore: !Deno.env.get("TEST_SERVER"),
   async fn() {
-    const res = await fetch("http://localhost:8080/api/v1/user/1");
-    const html = await res.text();
+    const res = await fetch("http://localhost:8080/");
+        const html = await res.text();
     assertEquals(res.status, 200);
     assertEquals(html.length > 1000, true);
   },
