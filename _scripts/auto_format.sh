f17938c76c627ab44a1a01e7bda5b90c2d29bf56
Jupegarnica <juan@geekshubs.com>
2022-03-19 17:27:11 +0100

    feat: better autoformat

--- _scripts/auto_format.sh
+++ _scripts/auto_format.sh
@@ -3,11 +3,13 @@
 # This script is used to automatically format the source code.
 
 echo "Formatting source code..."
-git stash --include-untracked --all save "Formatting source code"
+git stash --include-untracked --all
 
 # deno fmt
 
 FILES=$(deno fmt)
+
+  read -p "Do you want to apply the changes? [y/n] " -n 1 -r
 
 if [ -n "$FILES" ]; then
     echo "Committing changes..."
