8bb3107e99f9c017196fbbd98e44ebb964f8b030
Jupegarnica <juan@geekshubs.com>
2022-03-19 11:24:07 +0100

    feat: test and bench

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,4 +1,6 @@
-import logger from "https://deno.land/x/garn_logger@0.0.14/mod.ts";
+import logger, { console } from "https://deno.land/x/garn_logger@0.0.15/mod.ts";
 
-logger.setFilter(Deno.env.get("LOG_LEVEL") || "info");
+logger.setFilter(Deno.env.get("LOG_LEVEL") || "debug");
+
 export default logger;
+export { console };
