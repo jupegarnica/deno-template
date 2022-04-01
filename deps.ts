acc89d96d5213ea557ec5a23a80120428a1fc85a
GitHub <noreply@github.com>
2022-04-01 00:29:05 +0000

    chore(deps): update deno dependencies

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-// export { parse } from "https://deno.land/std@0.130.0/flags/mod.ts";
-export { serve } from "https://deno.land/std@0.130.0/http/server.ts";
-export type { ConnInfo } from "https://deno.land/std@0.130.0/http/server.ts";
+// export { parse } from "https://deno.land/std@0.133.0/flags/mod.ts";
+export { serve } from "https://deno.land/std@0.133.0/http/server.ts";
+export type { ConnInfo } from "https://deno.land/std@0.133.0/http/server.ts";
