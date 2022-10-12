49d71660233ba12313be198e8d2755080d84e9f4
Jupegarnica <juan@geekshubs.com>
2022-10-12 17:17:05 +0200

    feat: add json-server

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,57 @@
-import { parse } from "https://deno.land/std@0.158.0/flags/mod.ts";
+import { Cell, Row, Table } from "https://deno.land/x/cliffy@v0.25.2/table/mod.ts";
 
-const parsedArgs = parse(Deno.args);
-console.dir(parsedArgs);
-export default 42;
+new Table()
+  .header(Row.from(["Name", "Date", "City", "Country"]).border(true))
+  .body([
+    [
+      "Baxter Herman",
+      new Cell("Oct 1, 2020").border(true),
+      "Row 1 Column 3",
+      "Harderwijk",
+      "Slovenia",
+    ],
+    new Row("Jescie Wolfe", "Dec 4, 2020", "Alto Hospicio", "Japan").border(
+      true,
+    ),
+    ["Allegra Cleveland", "Apr 16, 2020", "Avernas-le-Bauduin", "Samoa"],
+    ["Aretha Gamble", "Feb 22, 2021", "Honolulu", "Georgia"],
+  ])
+  .render();
+// import {
+//     Checkbox,
+//     Confirm,
+//     Number,
+//     prompt,
+//   } from "https://deno.land/x/cliffy@v0.25.2/prompt/mod.ts";
 
-// import { opn } from "https://denopkg.com/hashrock/deno-opn/opn.ts";
+//   const result = await prompt([{
+//     name: "animals",
+//     message: "Select some animals",
+//     type: Checkbox,
+//     options: ["dog", "cat", "snake"],
+//   }, {
+//     name: "like",
+//     message: "Do you like animals?",
+//     type: Confirm,
+//     after: async ({ like }, next) => { // executed after like prompt
+//       if (like) {
+//         await next(); // run age prompt
+//       } else {
+//         await next("like"); // run like prompt again
+//       }
+//     },
+//   }, {
+//     name: "age",
+//     message: "How old are you?",
+//     type: Number,
+//     before: async ({ animals }, next) => { // executed before age prompt
+//       if (animals?.length === 3) {
+//         await next(); // run age prompt
+//       } else {
+//         await next("animals"); // begin from start
+//       }
+//     },
+//   }]);
 
-// const args = Deno.args.join(" ");
-
-// if (import.meta.main) {
-//   await opn(args);
-// }
+//   console.log(result);
+;
