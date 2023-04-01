92f712463cc0f32ceca06f95cfa79b78902cd598
GitHub <noreply@github.com>
2023-04-01 00:29:31 +0000

    chore(deps): update deno dependencies

--- test/deps.ts
+++ test/deps.ts
@@ -2,7 +2,7 @@ export {
   assert,
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.175.0/testing/asserts.ts";
+} from "https://deno.land/std@0.182.0/testing/asserts.ts";
 
 export { stub } from "https://deno.land/x/mock@0.15.2/mod.ts";
 export type { Stub } from "https://deno.land/x/mock@0.15.2/mod.ts";
